ed3e4ec4d983393160582dcb2fe6dbc8152a256a
Vitaliy-Yakovlev <vitaliy-yakovlev@ukr.net>
2021-07-19 14:59:06 +0300

    Исправил замечание ментора

--- src/App.js
+++ src/App.js
@@ -8,12 +8,7 @@ import Container from './components/Container';
 
 class Phponebook extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
 
@@ -24,16 +19,16 @@ class Phponebook extends Component {
       number,
     };
 
-    this.setState(({ contacts }) => ({
-      contacts: [contact, ...contacts],
-    }));
-
     const errorName = this.state.contacts.filter(
       contact => contact.name === name,
     );
+
     if (errorName.length) {
       alert(`${name} is already in contacts`);
-      return;
+    } else {
+      this.setState(({ contacts }) => ({
+        contacts: [contact, ...contacts],
+      }));
     }
   };
 
